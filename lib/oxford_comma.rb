e8f51de012e28beca93dcd74cd640e9626042789
DavidRMorphew <morphewd@umich.edu>
2020-09-11 18:49:11 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,28 @@
+# def oxford_comma(array)
+#     if array.count == 1
+#         array.join
+#     elsif array.count == 2
+#         array.insert(1, " and ")
+#         array.join 
+#     elsif array.count > 2
+#         final_element_removed = array.pop
+#         new_string = array.join(", ")
+#         new_string << ", and " + final_element_removed
+#     end
+# end
+
+# def oxford_comma(array)
+#     if array.count < 3
+#         array.join(" and ")
+#     else
+#         array[-1] = "and "+ array[-1]
+#         array.join(", ")
+#     end
+# end
+
 def oxford_comma(array)
-    if array.count == 1
-        array.join
-    elsif array.count == 2
-        array.insert(1, " and ")
-        array.join 
-    elsif array.count > 2
-        final_element_removed = array.pop
-        new_string = array.join(", ")
-        new_string << ", and " + final_element_removed
+    unless array.count == 1
+        array[-1] = "and " + array[-1]
     end
+    array.count >= 3 ? array.join(", ") : array.join(" ")
 end
