91c5a3794f151096ec3127e4ba3fe48bf26e667f
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 17:48:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+string = ""
+if array.length == 1
+  array.join("")
+elsif array.length == 2 
+  array.join(" and ")
+else array.each_with_index do |i, index|
+  if index < array.length - 1
+    array[index] = i+", "
+  else index == array.length - 1
+    array[index] = "and " + i
+  end
+end
+  array.join
+end
 end
