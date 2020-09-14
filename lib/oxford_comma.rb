a663b623abc2efbfd94d1d6344a563c5a80d387f
Kristian Joos <kjoos22@gmail.com>
2020-09-14 21:01:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    array.insert(1, " and ")
+    return array.join
+  elsif array.size > 2
+    index = 0
+    array.each do |element|
+      array[index] = element + ", "
+      index += 1
+    end
+    array[-1] = array.last.gsub(", ", "")
+    array.insert(array.size - 1, "and ")
+    return array.join
+  end
+  
 end
