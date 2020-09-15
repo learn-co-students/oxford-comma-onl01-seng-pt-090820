d62fa6f93a96b9ad0bcb443aaf0057cbd9b26a39
Carl J <carlj1881@yahoo.com>
2020-09-15 23:55:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
+require "pry"
 def oxford_comma(array)
-
+  #create edge case
+  if array.length <= 1
+    new_string = array.join
+  else if
+    array.length == 2
+    new_string = array.join(" and ")
+    
+  else if
+    array.length == 3
+    array[-1] = "and #{array.last}"
+        new_string = array.join(", ")
+        #binding.pry
+  else if
+    array.length > 3
+    array[-1] = "and #{array.last}"
+        new_string = array.join(", ")
+  end
+     end
+   end
+ end
 end
+
+
+#if array.length > 3
+  
+  
+
