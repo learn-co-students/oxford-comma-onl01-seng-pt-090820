2f89d16451ce4bd510a897ca38c0fab784444eab
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-19 02:42:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  puts array.to_s 
 end
+
+
+#   if array == [1] 
+#     puts  #{first.join(", ")}
+#   second
+# end 
+
+
+#   counter = 0 
+#   while counter < 3 
+#   array.join
+#   counter += 1 
+# end 
+# end 
+# end 
+
+
