fb2428884e859d153121209a9cf7d37a63bb964a
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-19 21:02:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,13 @@
+require "pry"
 def oxford_comma(array)
   if array.length == 1 
-  array.join 
+   array.join 
   elsif array.length == 2
-  array.join(" and ")
-elsif array.length == 3
-  array.join (", ")
- else [array, b, c].insert(2, "and")
-end 
+   array.join(" and ") 
+  else 
+  binding.pry
+  array.insert(array.length-1, "and").join(", ")
+ end 
 end 
 # oxford_comma(["kiwi"])).to eq("kiwi")
 # oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
