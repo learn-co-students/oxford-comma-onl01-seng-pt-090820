b56b4712a9ad43fc5fa1988966b2f43b845c261c
tmurrell2020 <littlerell12@gmail.com>
2020-09-18 21:23:59 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def oxford_comma(array)
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array[0..-2].join(", ") << ", and #{array[-1]}"
+    else
+        array.join
+    end
 
 end
