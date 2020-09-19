82e0174b5e7f1136dd3e90a5bd0d35aeee6cbda0
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-19 20:49:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.length == 1 
   array.join 
   elsif array.length == 2
-  puts  [array, "b"].join(" and ")
+  array.join(" and ")
 elsif array.length == 3
-  puts  [array, a, b].join (", ")
+  array.join (", ")
  else [array, b, c].insert(2, "and")
 end 
 end 
